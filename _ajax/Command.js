623e9fd153a39514d901794435c30300bd157a6b
Iliass Raihani <i.raihani@outlook.com>
2020-08-22 16:30:31 +0100

    fix content white space

--- _ajax/Command.js
+++ _ajax/Command.js
@@ -87,7 +87,7 @@ Command.entry.bind('keydown',function(e){
                 if(e.keyCode == 13 && e.shiftKey == true){
                     var content = $(this).val();
 
-                    Command.vim(content, splitValue[1], splitValue[2]);
+                    Command.vim(content.replace(/\s/g,''), splitValue[1], splitValue[2]);
                     Command.entry.val('');
                     $('.msg-group').html(output);
                 }
