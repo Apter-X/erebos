0a5a0307c187633045f4abd2fc369d8629cf272b
Iliass Raihani <i.raihani@outlook.com>
2020-08-27 18:28:24 +0100

    remove font

--- includes/head.php
+++ includes/head.php
@@ -3,5 +3,5 @@
 
 <!--CSS styles-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700|VT323" rel="stylesheet">
+<!-- <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700|VT323" rel="stylesheet"> -->
 <link rel="stylesheet" href="asset/css/style.css">
