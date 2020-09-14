49fead46fdacdaa1adec5bd77e2134a3ece66455
Iliass Raihani <i.raihani@outlook.com>
2020-09-14 08:09:50 +0100

    store footer

--- client/_ajax/template.php
+++ client/_ajax/template.php
@@ -20,12 +20,9 @@ if (isset($_POST['isOn']) && !empty($_POST['isOn'])) {
         echo htmlspecialchars_decode($database->fetchContent('interests'));
         echo htmlspecialchars_decode($database->fetchContent('awards'));        
     ?>
-        <!-- Bootstrap core JS-->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-        <!-- Third party plugin JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="assets/js/scripts.js"></script>
+    <?php
+        echo htmlspecialchars_decode($database->fetchContent('foot01'));
+    ?>
     </body>
     <?php
 }
