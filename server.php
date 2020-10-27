489aa4524af9800a94e3c476a31f3dff2d610650
Iliass Raihani <i.raihani@outlook.com>
2020-10-27 01:07:17 +0100

    add crono tool

--- server.php
+++ server.php
@@ -8,6 +8,15 @@
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
+        <!-- <div class="chrono">
+            <a href="#">chrono</a>
+            <span id="chronotime">0:00:00:00</span>
+            <form name="chronoForm">
+                <input type="button" name="startstop" value="start!" onClick="chronoStart()" />
+                <input type="button" name="reset" value="reset!" onClick="chronoReset()" />
+            </form>
+        </div> -->
+
         <div class="debug float-right"></div>
         <div class="msg-group">
             <!-- <div class="card-body">
@@ -25,5 +34,6 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="ajax/jsSide.js"></script> 
+        <script src="assets/js/chrono.js"></script>
     </body>
 </html>
