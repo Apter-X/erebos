6a3d5922b2d58318563fd7ce80f984a48e022e94
Iliass Raihani <i.raihani@outlook.com>
2020-10-27 21:19:46 +0100

    update pattern

--- index.php
+++ index.php
@@ -25,12 +25,12 @@
         </div>
         
         <div class="input-group">
-            <button class="before-input" disabled>></button>
+            <button class="before-input" disabled></button>
             <input id="input-box" class="form-control" rows="1" placeholder="Command Line" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" autofocus>
         </div>
         
         <!-- javascript -->
-        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/lib/jquery.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="ajax/jsSide.js"></script> 
